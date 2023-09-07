5906196b02698cfb873883e93bb615ea409c5ac0
Falson <Falson@DESKTOP-LLVB17J>
2023-09-06 22:44:36 -0500

    Adjusted database, connected controller back to hello-view, created databasecontroller to handle interaction between entry-view and SQLite database

---
+++ src/main/java/com/falson/labinventorymanager/DatabaseController.java
@@ -0,0 +1,8 @@
+package com.falson.labinventorymanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+public class DatabaseController {
+
+}
--- src/main/java/com/falson/labinventorymanager/HelloController.java
+++ src/main/java/com/falson/labinventorymanager/HelloController.java
@@ -2,13 +2,20 @@ package com.falson.labinventorymanager;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button buttonAddEntry;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    private void onAddLocationButtonClick() {
+
     }
 }
