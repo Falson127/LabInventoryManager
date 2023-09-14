6c327071e317caa81460cd671316d7afaa4a12d8
Falson <Falson@DESKTOP-LLVB17J>
2023-09-13 21:49:29 -0500

    database functions should be okay now. Next step is pulling info from DB into UI for viewing

--- src/main/java/com/falson/labinventorymanager/DatabaseController.java
+++ src/main/java/com/falson/labinventorymanager/DatabaseController.java
@@ -1,35 +1,76 @@
 package com.falson.labinventorymanager;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import javafx.scene.control.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DatabaseController {
     private Connection connection;
     @FXML
+    private TextField addEntry_Name;
+    @FXML
+    private TextField addEntry_Description;
+    @FXML
+    private TextField addEntry_Category;
+    @FXML
+    private ChoiceBox<String> addEntry_Location;
+    @FXML
+    private DatePicker addEntry_Date;
+    @FXML
+    private TextField addLocation_Name;
+    @FXML
+    private TextField addLocation_Parent;
+    @FXML
+    private TextField addLocation_Room;
+
+    @FXML
     private void onSubmitLocationButtonClick(){
-        String locationName = "placeholder";
-        int iD = 1;
-        String parent = "placeholder";
-        String room = "placeholder";
+        String locationName = addLocation_Name.getText() ;
+        String parent = addLocation_Parent.getText();
+        String room = addLocation_Room.getText();
         try{
             String url = "jdbc:sqlite:LabInventory.sqlite";
             connection = DriverManager.getConnection(url);
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO locations_index(Name, ID, Parent, Room) VALUES(?,?,?,?)");
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO locations_index(Name, Parent, Room) VALUES(?,?,?)");
             preparedStatement.setString(1,locationName);
-            preparedStatement.setInt(2,iD);
-            preparedStatement.setString(3,parent);
-            preparedStatement.setString(4,room);
-
+            preparedStatement.setString(2,parent);
+            preparedStatement.setString(3,room);
             preparedStatement.executeUpdate();
             preparedStatement.close();
             connection.close();
         }
         catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void onSubmitEntryButtonClick(){
+        String locationName = addEntry_Location.getValue();
+        String Name = addEntry_Name.getText();
+        String Category = addEntry_Category.getText();
+        String Description = addEntry_Description.getText();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+
+        try {
+            LocalDate Date = addEntry_Date.getValue();
+            String DateString = Date.format(formatter);
+            String url = "jdbc:sqlite:LabInventory.sqlite";
+            connection = DriverManager.getConnection(url);
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Item_Locations(Name,Category,Description,LocationName,DateReceived) VALUES(?,?,?,?,?)");
+            preparedStatement.setString(1,Name);
+            preparedStatement.setString(2,Category);
+            preparedStatement.setString(3,Description);
+            preparedStatement.setString(4,locationName);
+            preparedStatement.setString(5,DateString);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/com/falson/labinventorymanager/HelloController.java
+++ src/main/java/com/falson/labinventorymanager/HelloController.java
@@ -1,21 +1,17 @@
 package com.falson.labinventorymanager;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-    @FXML
-    private Button buttonAddEntry;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
+
     @FXML
     private void onAddLocationButtonClick() {
-
+    }
+    @FXML
+    private void onAddEntryButtonClick(){
     }
 }
