3c09af228bff6525e192c607c78aef4eba07db5e
Falson <benmlan80@gmail.com>
2023-09-23 21:48:28 -0500

    Added TreeCellFactory to handle extracting names from locations to display in the tree

---
+++ src/main/java/com/falson/labinventorymanager/LocationTreeCell.java
@@ -0,0 +1,17 @@
+package com.falson.labinventorymanager;
+
+import javafx.scene.control.TreeCell;
+
+public class LocationTreeCell extends TreeCell<Location> {
+    @Override
+    protected void updateItem(Location item, boolean empty){
+        super.updateItem(item, empty);
+
+        if(empty || item == null){
+            setText(null);
+            setGraphic(null);
+        } else{
+            setText(item.getName());
+        }
+    }
+}
